f97575908d30c5652ec8897fd77bb09a1e88c3ec
michaeljymsgutierrrez <leeez711@gmail.com>
2018-07-21 20:55:57 +0800

    Refactor webhook script

--- webhook.js
+++ webhook.js
@@ -7,69 +7,57 @@
 require("dotenv").config();
 const login = require("facebook-chat-api");
 const shell = require("shelljs");
+const ip = require("ip");
+const fastify = require("fastify")({
+  logger: true
+});
 
-if (
-  process.env.TITLE &&
-  process.env.EMAIL &&
-  process.env.PASSWORD &&
-  process.env.THREAD
-) {
-  /**
-   * Initialize data to send
-   */
-  let AUTHOR = null;
-  let DATE = null;
-  let HASH = null;
-  let COMMIT = null;
-  let BRANCH = null;
-  let POST_DATA = null;
-  let HEADER = process.env.TITLE;
+let host = ip.address();
+let port = "8080";
+let AUTHOR = null;
+let DATE = null;
+let HASH = null;
+let COMMIT = null;
+let BRANCH = null;
+let POST_DATA = null;
+let HEADER = process.env.TITLE;
 
-  /**
-   *  Function for executing shell command
-   */
-  let NODE_EXEC = command => {
-    let result = shell.exec(command);
-    if (result.code === 0) {
-      return result.stdout;
-    }
-  };
-
-  AUTHOR = NODE_EXEC("git show --summary | grep Author")
-    .replace(/\S+@\S+\.\S+/, "")
-    .trim();
-  DATE =
-    "Date: " +
-    NODE_EXEC("git show --summary | grep Date")
-      .replace("Date: ", "")
-      .trim();
-  HASH =
-    "Hash: " +
-    NODE_EXEC("git show --summary | grep commit")
-      .replace("commit", "")
-      .trim();
-  COMMIT = "Commit: " + NODE_EXEC("git log -1 --pretty=%B").trim();
-  BRANCH =
-    "Branch: " +
-    NODE_EXEC('git branch | grep "*"')
-      .replace("*", "")
-      .trim();
+fastify.post("/integrate/webhook", function(request, reply) {
+  let body = request.body;
+  console.log(body);
+  AUTHOR = "Author: " + body.commits[0].author.name;
+  BRANCH = "Branch: " + body.ref.replace("refs/heads/", "");
+  DATE = "Date: " + body.commits[0].timestamp;
+  HASH = "Hash: " + body.commits[0].id;
+  COMMIT = "Commit: " + body.commits[0].message;
   POST_DATA = `\n${HEADER}\n\n${BRANCH}\n${COMMIT}\n${AUTHOR}\n${DATE}\n${HASH}\n`;
 
-  login(
-    {
-      email: process.env.EMAIL,
-      password: process.env.PASSWORD
-    },
-    (err, api) => {
-      if (err) {
-        return console.error(err);
-      } else {
-        var yourID = process.env.THREAD;
-        api.sendMessage(POST_DATA, yourID);
+  if (
+    process.env.TITLE &&
+    process.env.EMAIL &&
+    process.env.PASSWORD &&
+    process.env.THREAD
+  ) {
+    login(
+      {
+        email: process.env.EMAIL,
+        password: process.env.PASSWORD
+      },
+      (err, api) => {
+        if (err) {
+          return console.error(err);
+        } else {
+          var yourID = process.env.THREAD;
+          api.sendMessage(POST_DATA, yourID);
+        }
       }
-    }
-  );
-} else {
-  console.log("Oops!, there is something wrong with your '.env' file . . .");
-}
+    );
+  } else {
+    console.log("Oops!, there is something wrong with your '.env' file . . .");
+  }
+});
+
+fastify.listen(port, host, (err, address) => {
+  if (err) throw err;
+  fastify.log.info(`server listening on ${address}`);
+});
